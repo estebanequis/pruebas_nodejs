a536606f90c47e06e53588071b2fa85008e84e65
esteban <estebanequis@gmail.com>
2018-09-05 21:21:10 -0300

    con puerto heroku

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-/*
 var http = require('http');
 var server = http.createServer();
+
+var port = process.env.PORT || 8080;
+
 function control(petic, resp) {
 resp.writeHead(200, {'content-type': 'text/plain'});
 resp.write('Hola, Mundo!');
 resp.end();
 }
 server.on('request', control);
-server.listen(80);
-*/
-
-console.log("hola trolo");
+server.listen(port);
